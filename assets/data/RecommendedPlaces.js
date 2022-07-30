e68a5cb903e48154bfaa8e74774057f430e7b9c4
Leead Arbetman <liad180895@gmail.com>
2022-07-30 17:08:55 +0300

    Updating data

---
+++ assets/data/RecommendedPlaces.js
@@ -0,0 +1,32 @@
+const recommendedPlaces = [
+    {
+        id: 1,
+        image: require('../images/Habima-Squere.jpg'),
+        title: 'Habima Squere',
+        rate: '5',
+        selected: true,
+    },
+    {
+        id: 2,
+        image: require('../images/Ramat-Aviv-Mall.jpg'),
+        title: 'Ramat Aviv Mall',
+        rate: '4.8',
+        selected: true,
+    },
+    {
+        id: 3,
+        image: require('../images/Rabin-Squere.jpg'),
+        title: 'Rabin Squere',
+        rate: '4.5',
+        selected: true,
+    },
+    {
+        id: 4,
+        image: require('../images/Dizengoff-Center.jpg'),
+        title: 'Dizengoff Center',
+        rate: '4.5',
+        selected: true,
+    }
+]; 
+
+export default recommendedPlaces;
--- assets/data/favoritesData.js
+++ assets/data/favoritesData.js
@@ -1,32 +1,32 @@
 const favoritsData = [
     {
         id: 1,
-        image: require('../images/azrieli-mall.png'),
-        title: 'Azrieli-Holon',
-        rate: '4.5',
+        image: require('../images/Habima-Squere.jpg'),
+        title: 'Habima Squere',
+        rate: '5',
         selected: true,
     },
     {
         id: 2,
-        image: require('../images/azrieli-mall.png'),
-        title: 'Azrieli-TelAviv',
-        rate: '3',
+        image: require('../images/Rabin-Squere.jpg'),
+        title: 'Rabin Squere',
+        rate: '4.8',
         selected: true,
     },
     {
         id: 3,
-        image: require('../images/azrieli-mall.png'),
-        title: 'Azrieli-RamatGan',
-        rate: '4',
+        image: require('../images/Academic-Tel-Aviv-Yafo.jpg'),
+        title: 'Academic College',
+        rate: '4.7',
         selected: true,
     },
     {
         id: 4,
-        image: require('../images/azrieli-mall.png'),
-        title: 'Azrieli-BatYam',
-        rate: '5',
+        image: require('../images/Neve-Tzedek.jpg'),
+        title: 'Neve Tzedek',
+        rate: '3.9',
         selected: true,
-    }
+    },
 ]; 
 
 export default favoritsData;
