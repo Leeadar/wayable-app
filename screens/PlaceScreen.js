dd5e0eb212a0a92efcda05bd2bda709e87a7dfe9
NatiDvir <natithedvir@gmail.com>
2022-08-14 13:47:41 +0300

    PlaceScreen FINITO ALMOST

--- screens/PlaceScreen.js
+++ screens/PlaceScreen.js
@@ -7,6 +7,7 @@ import favoritesData from '../assets/data/favoritesData';
 import highlyRatedData from '../assets/data/highlyRatedData';
 import { db } from '../Core/Config';
 import { getDatabase, ref, set, onValue, update } from "firebase/database";
+import { stringLength } from '@firebase/util';
 
 
 
@@ -54,7 +55,7 @@ const  PlaceScreen =  ({ route, navigation })=>{
                       });
     }
 
-    console.log(replaced) // will print the fixed place name
+    //console.log(replaced) // will print the fixed place name
 
 
     // for example: if data base will contain /places/Azrieli_Mall , Then replaced = "Azrieli_Mall". 
@@ -83,23 +84,127 @@ const  PlaceScreen =  ({ route, navigation })=>{
             }
         });
     }
-
+   // console.log(averageRating)
   React.useEffect(()=>{ readData()})
     
     return (
-        <View on style={styles.container}>
+        // <View on style={styles.container}>
                          
 
-                <Text>Name: {place}</Text> 
-                <Text>Parking rate: {parking}</Text> 
-                <Text>Wheelchair access rate: {wheelchair_access}</Text> 
-                <Text>Way to place rate: {way_to_place}</Text>
-                <Text>Door access rate: {door_access}</Text>
-                <Text>Stairs alternative rate: {stairs_alternative}</Text>
-                <Text>Toilets rate: {toilets}</Text> 
-                <Text>number of ratings: {numOfRatings}</Text> 
-                <Text>Average rating: {averageRating}</Text> 
+        //         <Text>Name: {place}</Text> 
+        //         <Text>Parking rate: {parking}</Text> 
+        //         <Text>Wheelchair access rate: {wheelchair_access}</Text> 
+        //         <Text>Way to place rate: {way_to_place}</Text>
+        //         <Text>Door access rate: {door_access}</Text>
+        //         <Text>Stairs alternative rate: {stairs_alternative}</Text>
+        //         <Text>Toilets rate: {toilets}</Text> 
+        //         <Text>number of ratings: {numOfRatings}</Text> 
+        //         <Text>Average rating: {averageRating}</Text> 
                  
+
+        // </View>
+
+
+        <View>
+               {/* Header */}
+               <SafeAreaView>
+                    <View style={styles.headWrapper}>
+                        <Image source={require('../assets/images/Wayable.png')} 
+                        style={styles.headImage}
+                         />                       
+                    </View>
+                </SafeAreaView>
+                {/* TODO : REQUIRE IMAGE FROM GOOGLE API */}
+                <Image style={styles.imageStyle} source={require('../assets/images/azrieli-mall.png')} ></Image>
+
+                {/* Place Name */}
+                <View style={styles.placeNameWrapper}> 
+                    <Text style = {styles.placeNameStyle}>{place}</Text>
+                </View>
+
+                {/* Average Rating */}
+                <View style={styles.RatingWrapper1}>
+                    <View styles={styles.RatingWrapper2}>
+                        <Text style={styles.ratingText}>{averageRating}</Text>
+                    </View>
+                    <View>
+                        <Image style={styles.ratingImage} source={require('../assets/images/rate.png')}></Image>
+                    </View>
+                </View>
+
+
+
+                {/* TO DO : Rating Button & Navigate Button */}
+
+
+
+                {/* All Ratings: */}
+                <View style={styles.RatingsWrapper}>
+
+                        {/* Parking rating */}
+                    <View style={styles.ParkingWrapper}>
+                        <Image style={styles.ParkingImage} source={require('../assets/images/parking.png')} ></Image>
+                        <Text style={styles.DoorAccess}>Parking</Text>
+                        <View style={{flexDirection:'row', alignItems:'center'}}>
+                            <Text style={styles.DoorRate}>{parking}</Text>
+                            <Image style={styles.DoorStar} source={require('../assets/images/rate.png')}></Image>
+                        </View>
+                    </View>
+
+                        {/* WheelChair rating */}
+                    <View style={styles.WheelchairWrapper}>
+                        <Image style={styles.ParkingImage} source={require('../assets/images/wheelchair.png')} ></Image>
+                        <Text style={styles.DoorAccess}>Wheelchair access</Text>
+                        <View style={{flexDirection:'row', alignItems:'center'}}>
+                            <Text style={styles.DoorRate}>{wheelchair_access}</Text>
+                            <Image style={styles.DoorStar} source={require('../assets/images/rate.png')}></Image>
+                        </View>
+                    </View>
+
+                        {/* Way rating */}
+                    <View style={styles.WayWrapper}>
+                        <Image style={styles.ParkingImage} source={require('../assets/images/way.png')} ></Image>
+                        <Text style={styles.DoorAccess}>Way to place</Text>
+                        <View style={{flexDirection:'row', alignItems:'center'}}>
+                            <Text style={styles.DoorRate}>{way_to_place}</Text>
+                            <Image style={styles.DoorStar} source={require('../assets/images/rate.png')}></Image>
+                        </View>
+                    </View>
+
+                      {/* Door rating */}
+                    <View style={styles.DoorWrapper}>
+                        <Image style={styles.ParkingImage} source={require('../assets/images/door.png')} ></Image>
+                        <Text style={styles.DoorAccess}>Door Access</Text>
+                        <View style={{flexDirection:'row', alignItems:'center'}}>
+                            <Text style={styles.DoorRate}>{door_access}</Text>
+                            <Image style={styles.DoorStar} source={require('../assets/images/rate.png')}></Image>
+                        </View>
+                    </View>
+
+                        {/* Stairs rating */}
+                    <View style={styles.StairsWrapper}>
+                        <Image style={styles.ParkingImage} source={require('../assets/images/stairs.png')} ></Image>
+                        <Text style={styles.DoorAccess}>Stairs Alternative</Text>
+                        <View style={{flexDirection:'row', alignItems:'center'}}>
+                            <Text style={styles.DoorRate}>{stairs_alternative}</Text>
+                            <Image style={styles.DoorStar} source={require('../assets/images/rate.png')}></Image>
+                        </View>
+                    </View>
+
+                           {/* Toilets rating */}
+                    <View style={styles.ToiletsWrapper}>
+                        <Image style={styles.ParkingImage} source={require('../assets/images/toilet.png')} ></Image>
+                        <Text style={styles.DoorAccess}>Toilets</Text>
+                        <View style={{flexDirection:'row', alignItems:'center'}}>
+                            <Text style={styles.DoorRate}>{toilets}</Text>
+                            <Image style={styles.DoorStar} source={require('../assets/images/rate.png')}></Image>
+                        </View>
+                    </View>
+
+                </View>
+
+
+
 
         </View>
     )
@@ -115,12 +220,148 @@ const styles = StyleSheet.create({
     },
     textbox:{ 
         width: '90%',
-        fontSize: 18,
+        fontSize: 16,
         padding:12,
         borderColor: 'gray',
         borderWidth:0.2,
         borderRadius:10,
-    }
-})
+    },
+    headWrapper:{
+        position: "absolute",
+        left:96,
+        top:40,
+        borderRadius:null,
+        width:188,
+        height:24,
+    },
+    imageStyle:{
+        position:"absolute",
+        alignContent:"center",
+        left: 5,
+        top:100,
+        borderRadius: 10,
+        width: 400,
+        height:196,
+    },
+    placeNameWrapper:{
+        position:"absolute",
+        left:16,
+        top:300,
+        width:189,
+        height:29,
+    },
+    placeNameStyle:{
+        position:"absolute",
+        left:0,
+        right:-2,
+        top:0,
+        width:"auto",
+        fontSize:28,
+        letterSpacing: -0.5,
+        
+        textAlign: "left",
+    },
+    RatingWrapper1:{
+        position:"absolute",
+        left:16,
+        top:335,
+        width:96,
+        height:45,
+    },
+    DoorStar:{
+        marginLeft:5,
+    },
+    RatingWrapper2:{
+        position:"absolute",
+        left:0,
+        top:5,
+        width:48,
+        height:39,
+    },
+    ratingText:{
+        position:"absolute",
+        left:0,
+        top:4,
+        width:50,
+        fontSize:32,
+        letterSpacing:-0.5,
+        textAlign:"left",
+    },
+    ratingImage:{
+        position:"absolute",
+        left:50,
+        top:0,
+        borderRadius:null,
+        width:45,
+        height:45,
+
+    },
+    RatingsWrapper:{
+        position:"absolute",
+        left:1,
+        top:387,
+        width:375,
+        height:339,
+        borderColor:'black',
+        borderTopColor:`#add8e6`,
+        borderTopWidth:1,
+        borderTopLeftRadius:30
+        
+        
+    },
+    DoorWrapper:{
+        position:"absolute",
+        left:10,
+        top:170,
+        width:104,
+        height:133,
+    },
+    StairsWrapper:{
+        position:"absolute",
+        left:140,
+        top:170,
+        width:200,
+        height:133,
+    },
+    ToiletsWrapper:{
+        position:"absolute",
+        left:310,
+        top:170,
+        width:200,
+        height:133,
+    },
+    ParkingWrapper:{
+        position:"absolute",
+        left:10,
+        top:67,
+        width:200,
+        height:133,
+    },
+    WheelchairWrapper:{
+        position:"absolute",
+        left:140,
+        top:67,
+        width:200,
+        height:133,
+    },
+    WayWrapper:{
+        position:"absolute",
+        left:310,
+        top:67,
+        width:200,
+        height:133,
+    },
+    ParkingImage:{
+        position:"absolute",
+        top: -50,
+        left:0,
+        height:50,
+        width:50,      
+    },
+    
+    
+}
+)
+
 
 export default PlaceScreen;
