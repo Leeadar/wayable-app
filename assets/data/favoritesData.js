0fa0c042acc1513b22fd6b9ad4215db70cc8e4b2
Leead Arbetman <liad180895@gmail.com>
2022-09-18 08:52:31 +0300

    small updates

--- assets/data/favoritesData.js
+++ assets/data/favoritesData.js
@@ -8,7 +8,7 @@ const favoritsData = [
         id: 1,
         image: require('../images/Habima-Squere.jpg'),
         title: 'Habima Square',
-        rate: 4.33,
+        rate: 4.22,
         selected: true,
         photoReference:'AeJbb3dt93RCqkz3PFk9w7UzRr6Vmtu2L23ICkBuyN_WHEtygOsRF__hUzMMoxmfLmiPf2-aJGXKNCbZemh9k7rvD3mnvfrlKiTU3X-R10h2DTkA_vmbF-ULmQAdFimtqT32RVDVp6dfW4KFIEZAF6okw8X43l8_Ga-ddi4h4mp6d7xBzcUR'
     },
@@ -24,7 +24,7 @@ const favoritsData = [
         id: 3,
         image: require('../images/Academic-Tel-Aviv-Yafo.jpg'),
         title: 'Academic College',
-        rate: '3.88',
+        rate: '4',
         selected: true,
         photoReference:'AeJbb3fjewtiFUT8FqgnTpym_SGnQ2Uyw--Rl0wafuUg4gPpcKqTVXP2y0oWqn9d43jwMW_yA8RlbvIukGkJf6BMTX6nBkXGpAw-x4em6l4Jqa-cHqSRYJpnZr20G8IDlynfLb23-02MoIgbUWoWOt7XTCZp1fNNQv-CeUgglaoyattx4PEV'
     },
@@ -32,7 +32,7 @@ const favoritsData = [
         id: 4,
         image: require('../images/Neve-Tzedek.jpg'),
         title: 'Neve Tzedek',
-        rate: '3.9',
+        rate: '3.5',
         selected: true,
     },
 ]; 
--- assets/data/highlyRatedData.js
+++ assets/data/highlyRatedData.js
@@ -3,7 +3,7 @@ const highlyRatedData = [
         id: 1,
         image: require('../images/Azrieli-Center.jpg'),
         title: 'Azrieli Mall',
-        rate: '5',
+        rate: '4.2',
         selected: true,
         photoReference:'AeJbb3eiUwYTzSnRjopAjRSDzEvMmKTpsjS6Jt2IaweTODuRjpjjXURyUPLTr4kdis6b-t8hYO6lCYWVbRGDdo1qZvTIZa6d_0iN3gPHPhuqxyEwLSfKxkbmdjzqH3dCbIyGcDmDJkY49Cov8CyAkWgdRkXXTitkCDzmpybUOPKbRt-p-3ah'
     },
@@ -11,7 +11,7 @@ const highlyRatedData = [
         id: 2,
         image: require('../images/Tel-Aviv-Museum-Of-Art.jpg'),
         title: 'Tel Aviv Art Museum',
-        rate: '4.8',
+        rate: '4.2',
         selected: true,
         photoReference:'AeJbb3ehtfhqKPsCRVSegUU2A3Wy67w3UlCMwhc6VycfBWxj8_kCEG2vxD_GDMGw5l5XX_6k-JksWdQA1qvrPJmrC9FXVG4mvrEIMgXMJocWCjBBFbD1OZZCT4SgSZwY0vhasjwmkZrwO0AEEQt3pOCZQrQ4Oeb1baw48_WmFsR1rKH6Arby'
     },
@@ -19,7 +19,7 @@ const highlyRatedData = [
         id: 3,
         image: require('../images/Rabin-Squere.jpg'),
         title: 'Rabin Square',
-        rate: '4.5',
+        rate: '3.8',
         selected: true,
         photoReference:'AeJbb3frYkf6np6km5F24s_9l6DlG3SC2t2e8lGm5cJoC30cMO5XFRFUVL-BQaEj4IBbo1kIPZEZZZP16bS8IdfBRMciisVaBlivemfSh7O-ybK6UVhs5WFDouv5LyqIjOx3ya7TLMjuwhSqhCnrB_dTxZrpoTtOC-U9t9h8T3_zX5u6VDkl'
     },
@@ -27,7 +27,7 @@ const highlyRatedData = [
         id: 4,
         image: require('../images/Habima-Squere.jpg'),
         title: 'Habima Square',
-        rate: '4',
+        rate: '4.2',
         selected: true,
         photoReference:'AeJbb3dt93RCqkz3PFk9w7UzRr6Vmtu2L23ICkBuyN_WHEtygOsRF__hUzMMoxmfLmiPf2-aJGXKNCbZemh9k7rvD3mnvfrlKiTU3X-R10h2DTkA_vmbF-ULmQAdFimtqT32RVDVp6dfW4KFIEZAF6okw8X43l8_Ga-ddi4h4mp6d7xBzcUR'
     },
